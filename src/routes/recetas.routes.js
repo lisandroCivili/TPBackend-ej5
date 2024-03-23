e50aa75b85dc73ba2618aec6f710a0d816e9ad1e
Lisandro Civili <lisandrocivili13@gmail.com>
2024-03-23 04:50:17 -0300

    todas las peticiones configuradas

--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { crearReceta, listarRecetas } from "../controllers/recetas.controllers.js";
+import { borrarReceta, crearReceta, editarReceta, listarRecetas, obtenerReceta } from "../controllers/recetas.controllers.js";
 
 const router = Router()
 router.route('/recetas').get(listarRecetas).post(crearReceta)
+router.route('/recetas/:id').get(obtenerReceta).put(editarReceta).delete(borrarReceta)
 
 export default router
